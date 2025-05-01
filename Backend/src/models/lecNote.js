194907b31133de080fd2b98516c06550bb0d0e9a
IsuruKRanasundara <ranasundaraisuru4@gmail.com>
2025-05-01 23:42:30 +0530

    Create the lecnote model

---
+++ Backend/src/models/lecNote.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+const lecNoteSchema = new Schema({
+    _id: {
+        type: mongoose.Schema.Types.ObjectId,
+        auto: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    filePath: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+    createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User', // Reference to the User model
+    required: true,
+}
+
+});
+
+
+
+
+
+const LecNote = mongoose.model('LecNote', lecNoteSchema);
+module.exports = LecNote;
+
+
